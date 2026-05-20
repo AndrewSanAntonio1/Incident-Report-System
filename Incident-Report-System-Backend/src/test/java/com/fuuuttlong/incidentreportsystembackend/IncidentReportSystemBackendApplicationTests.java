0e13dce9166273c725582ceda764c61ce7398f81
AndrewSanAntonio1 <sgandrew290@gmail.com>
2026-05-20 19:04:05 +0800

    Fix nested repository

---
+++ Incident-Report-System-Backend/src/test/java/com/fuuuttlong/incidentreportsystembackend/IncidentReportSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuuuttlong.incidentreportsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentReportSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
